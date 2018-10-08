026744f3e22e04b4550ec96164bc572820c47012
Rodrigo Campeão <servicovivi732559@userede.com.br>
2018-10-08 17:21:44 +0000

    SG added into elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,7 @@
 resource "aws_elb" "myproject" {
 	name				= "terraform-asg-myproject"
 	availability_zones		= ["data.aws_availability_zones.all.names"]
+	security_groups			= ["${aws_security_group.elb.id}"]
 
 	listener {
 		lb_port			= "${var.elb_listener["ssl_port"]}"
